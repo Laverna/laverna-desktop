c9103189943c4ba671d6e87222440de344021966
wwebfor <wwebfor@yandex.com>
2017-06-06 15:58:55 +0600

    [Chore] Create build archives

--- res/build/index.js
+++ res/build/index.js
@@ -5,9 +5,13 @@
  */
 const packager = require('electron-packager');
 const path     = require('path');
-const options  = {
-    dir       : path.join(__dirname, '../../'),
-    out       : path.join(__dirname, '../../dist'),
+const exec     = require('child_process').execSync;
+const pkg      = require('../../package.json');
+
+const dir     = path.join(__dirname, '../..');
+const options = {
+    dir,
+    out       : path.join(dir, '/dist'),
     name      : 'Laverna',
     overwrite : true,
     arch      : 'all',
@@ -28,13 +32,34 @@ function createPackage(opt) {
     return new Promise(resolve => packager(opt, () => resolve()));
 }
 
+/**
+ * Create tar, zip archives.
+ */
+function createArchives() {
+    const archiveDir = `${dir}/dist/release-archives`;
+    const name       = `release-archives/laverna-${pkg.version}`;
+    exec(`rm -rf ${archiveDir} && mkdir ${archiveDir}`);
+    process.chdir(`${dir}/dist`);
+
+    // Linux
+    exec(`tar -czf ${name}-linux-x64.tar.gz ./Laverna-linux-x64`);
+    exec(`tar -czf ${name}-linux-ia32.tar.gz ./Laverna-linux-ia32`);
+
+    // Windows
+    exec(`zip -r ${name}-windows-x64.zip ./Laverna-win32-x64`);
+    exec(`zip -r ${name}-windows-ia32.zip ./Laverna-win32-ia32`);
+
+    // MacOS
+    exec(`tar -czf ${name}-macOS-x64.tar.gz ./Laverna-darwin-x64`);
+}
+
 Promise.all([
     createPackage(Object.assign({platform: 'linux'}, options)),
 
     // Build for Windows
     createPackage(Object.assign({
         platform             : 'win32',
-        icon                 : path.join(__dirname, '../../app/dist/favicon.ico'),
+        icon                 : path.join(dir, '/app/dist/favicon.ico'),
         win32metadata        : {
             CompanyName      : 'Laverna',
             FileDescription  : 'Laverna - note taking app',
@@ -47,12 +72,16 @@ Promise.all([
     // Build for MacOS
     createPackage(Object.assign({
         platform        : 'darwin',
-        icon            : path.join(__dirname, '../../app/dist/images/icon/icon-512x512.icns'),
+        icon            : path.join(dir, '/app/dist/images/icon/icon-512x512.icns'),
         appBundleId     : 'laverna.cc',
         appCategoryType : 'public.app-category.productivity',
     }, options)),
 ])
 .then(() => {
+    if (process.env.NODE_ENV === 'release') {
+        createArchives();
+    }
+
     console.log('Packages are created.');
     process.exit(0);
 });
